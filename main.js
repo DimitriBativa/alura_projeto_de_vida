14150b669f39beb71c4508b02bbc95e489df2191
DimitriBativa345 <dimitridecristo@gmail.com>
2025-05-21 17:48:40 -0300

    cursofeito

--- main.js
+++ main.js
@@ -47,9 +47,9 @@ function calculaTempo(tempoObjetivo){
 
 for (let i=0; i<contadores.length; i++){
     function atualizaCronometro(){
-        document.getElementById("dias"+i).textContent = calculaTempo(tempos[0])[0];
-        document.getElementById("horas"+i).textContent = calculaTempo(tempos[0])[1];
-        document.getElementById("min"+i).textContent = calculaTempo(tempos[0])[2];
+        document.getElementById("dias"+i).textContent = calculaTempo(tempos[i])[0];
+        document.getElementById("horas"+i).textContent = calculaTempo(tempos[i])[1];
+        document.getElementById("min"+i).textContent = calculaTempo(tempos[i])[2];
         document.getElementById("seg"+i).textContent = calculaTempo(tempos[i])[3];
     }
 }
